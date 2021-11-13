1fe890c1d0039a51ea3bd6abce88fed6a93ff924
pablom <pablo_89_cba@hotmail.com>
2021-11-13 12:12:30 -0300

    Mejorando algunas respuestas JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -59,21 +59,21 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  return x === y ? true : false;
+  return x === y;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return str1.length === str2.length ? true : false;
+  return str1.length === str2.length;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num < 90 ? true : false;
+  return num < 90;
 }
 
 function mayorQueCincuenta(num) {
@@ -99,20 +99,23 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  var numero;
+  /* 
+ var numero;
   if (num % 2 === 0) {
     numero = true;
   } else {
     numero = false;
   }
   return numero;
+  */
+  return num % 2 === 0;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  return !(num % 2 === 0) ? true : false;
+  return !(num % 2 === 0);
 }
 
 function elevarAlCuadrado(num) {
@@ -152,17 +155,16 @@ function numeroRandom() {
   return Math.random();
 }
 
-
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero > 0){
+  if (numero > 0) {
     return "Es positivo";
-  }else if (numero < 0){
+  } else if (numero < 0) {
     return "Es negativo";
-  }else{
+  } else {
     return false;
   }
 }
@@ -218,30 +220,30 @@ function esVocal(letra) {
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.lenght > 1 ){
+  if (letra.lenght > 1) {
     return "Dato incorrecto";
-  }else{
-  switch (letra) {
-    case "a":
-      return "Es vocal";
-      break;
-    case "e":
-      return "Es vocal";
-      break;
-    case "i":
-      return "Es vocal";
-      break;
-    case "o":
-      return "Es vocal";
-      break;
-    case "u":
-      return "Es vocal";
-      break;
-    default:
-      return "Dato incorrecto"
-      break;
+  } else {
+    switch (letra) {
+      case "a":
+        return "Es vocal";
+        break;
+      case "e":
+        return "Es vocal";
+        break;
+      case "i":
+        return "Es vocal";
+        break;
+      case "o":
+        return "Es vocal";
+        break;
+      case "u":
+        return "Es vocal";
+        break;
+      default:
+        return "Dato incorrecto";
+        break;
+    }
   }
- }
 }
 
 // No modificar nada debajo de esta línea
